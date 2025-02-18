1799a7994a80998866152c5d714a36c66a418370
rahulroynipon <rahulroynipon@gmail.com>
2025-02-18 12:11:59 +0600

    enhance MobileUsesSection with animations and improved layout for better user interaction

--- src/components/home/mobile/MobileUsesSection.jsx
+++ src/components/home/mobile/MobileUsesSection.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import Container from "../../global/Container";
 import { usesItems } from "../desktop/DeskUsesSection";
 import cn from "./../../../utils/cn";
@@ -16,42 +17,59 @@ function MobileUsesSection() {
       <h4 className="font-roboto text-3xl font-medium text-gray-800">
         Use Carepatron for
       </h4>
-      <section>
+      <section className="mt-8">
         <ul>
           {usesItems?.map((item, index) => (
             <li
               key={index}
               onClick={() => toggleActive(index)}
-              className="cursor-pointer mt-7 rounded-xl  mx-3"
+              className="py-2 px-1 md:p-5 space-y-5 mt-3 cursor-pointer"
             >
-              <div className="flex items-center justify-between">
-                <div className="flex items-center space-x-4">
-                  <div
-                    className={cn(
-                      "flex items-center p-4 w-12 rounded-xl shrink-0",
-                      item?.color
-                    )}
-                  >
-                    <img
-                      src={item.icon}
-                      alt={`icon-${index}`}
-                      className="h-6 w-6"
+              <div className="flex space-x-4 rounded-xl">
+                <div
+                  className={cn(
+                    "p-4 rounded-xl flex items-center justify-center",
+                    item?.color
+                  )}
+                >
+                  <img
+                    src={item.icon}
+                    alt={`uses-ion-${index}`}
+                    className="h-6 w-6"
+                  />
+                </div>
+                <div className="w-full">
+                  <div className="flex items-center justify-between text-gray-800">
+                    <h5 className="font-medium text-lg">{item.label}</h5>
+                    <IoIosArrowDown
+                      className={cn(
+                        "transition-transform duration-500 text-xl md:text-2xl",
+                        activeIndex === index ? "rotate-180" : "rotate-0"
+                      )}
                     />
                   </div>
-                  <div className="flex flex-col text-gray-800">
-                    <div className="flex items-center justify-between border w-full ">
-                      <h5 className="font-semibold text-lg">{item.label}</h5>
-                      <IoIosArrowDown className="" />
-                    </div>
-                    <p className="text-sm"> {item.description}</p>
-                  </div>
+                  <p className="text-sm md:text-base">{item?.description}</p>
                 </div>
               </div>
-              {activeIndex === index && (
-                <div className="mt-3 text-sm text-gray-600">
-                  <img src="" alt="" />
-                </div>
-              )}
+
+              <AnimatePresence initial={false}>
+                {activeIndex === index && (
+                  <motion.div
+                    initial={{ width: 0, height: 0 }}
+                    animate={{ width: "auto", height: "auto" }}
+                    exit={{ width: 0, height: 0 }}
+                    transition={{ duration: 0.4, ease: "easeInOut" }}
+                    style={{ originX: 0.5, originY: 0.5 }}
+                    className="flex items-center justify-center mx-auto"
+                  >
+                    <img
+                      src={item?.image}
+                      alt={`uses-image-${index}`}
+                      className="w-full h-auto"
+                    />
+                  </motion.div>
+                )}
+              </AnimatePresence>
             </li>
           ))}
         </ul>
